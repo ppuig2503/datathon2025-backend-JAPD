4b4a66805048a7d7238e0b0403b5c81dadad4a2c
JoanVM41 <joan.vicente02@estudiant.upf.edu>
2025-11-15 19:08:32 +0100

    Bash Script

---
+++ install_deps.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+pip install fastapi
+pip install uvicorn
+pip install pydantic
+pip install pandas
+pip install joblib
+pip install scikit-learn
+pip install lightgbm
+pip install openai
+pip install httpx
+pip install python-dotenv
+pip install scikit-image
+pip install lime
